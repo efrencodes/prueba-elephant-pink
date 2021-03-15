b9633f2e77c992ae7cd216dfca6e57ee5edff296
efrenmartinez <developer.emartinez@gmail.com>
2021-03-14 23:48:31 -0600

    Initial project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <img
+      src="https://res.cloudinary.com/efrenmartinez/image/upload/v1615782991/prueba-elephant-pink/logotipo_jhgagt.svg"
+      alt="Logotipo"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
